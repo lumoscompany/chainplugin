8f36372bdf9a6c22e897949d827f87c1afac93f9
Adam Stragner <adam@stragner.com>
2023-07-25 17:58:38 +0400

    Add `EventStatus` and method to update it

--- source/eventsProvider.ts
+++ source/eventsProvider.ts
@@ -29,6 +29,12 @@ type EventPreviewRegularSignificant = {
 }
 
 /**
+ * An event status,
+ * Can be `success` or 'pending` otherwise is error
+ */
+type EventStatus = 'success' | 'pending' | string
+
+/**
  * An event preview type
  */
 type EventPreviewSignificant = {} & (
@@ -44,19 +50,26 @@ type EventPreview = {
   complementary: ComplementaryField[]
 }
 
+type EventHash = {
+  /**
+   * An unique hash of the event
+   */
+  hash: string
+}
+
 /**
  * An type that represents an event.
  */
-type Event = {
+type Event = EventHash & {
   /**
    * Date of the event, seconds
    */
   date: number
 
   /**
-   * An unique hash of the event
+   * A type of event's status {@link EventStatus | EventStatus}
    */
-  hash: string
+  status: EventStatus
 
   /**
    * A type of event {@link EventType | EventType}
@@ -86,21 +99,6 @@ type Event = {
    * A visual preview of event {@link EventPreview | EventPreview}
    */
   preview: EventPreview
-}
-
-/**
- * A request to retrieve updated verions of event for specified account address
- */
-type EventsProviderUpdateRequest = {
-  /**
-   * An address of owner of the event
-   */
-  owner: string
-
-  /**
-   * Previous version of event that have been marked as `incomplete`
-   */
-  event: Event
 }
 
 /**
@@ -138,21 +136,29 @@ type EventsProviderFetchRequest = {
  */
 type EventsProvider = {
   /**
-   * Method that can be invoked by native code to update a event if it's property `incomplete` marked as `true`
+   * Method that can be invoked by native code to update an event if it's property `incomplete` marked as `true`
    *
    * @remarks
    * It strongly recommended to return event which field `incomplete` marked as `false` to avoid unnecessary network usage
    *
-   * @param args - {@link EventsProviderUpdateRequest | EventsProviderUpdateRequest}
+   * @param args - {@link EventHash | EventHash}
    * @returns Promise with updated {@link Event | Event}
    */
-  update(args: EventsProviderUpdateRequest): Promise<Event>
+  update(args: EventHash): Promise<Event>
+
+  /**
+   * Method that can be invoked by native code to update an event's status
+   *
+   * @param args - {@link EventHash | EventHash}
+   * @returns Promise with updated event's status {@link EventStatus | EventStatus}
+   */
+  status(args: EventHash): Promise<EventStatus>
 
   /**
    * Method that invoked by native code to fetch list of events for specified account address
    *
    * @remarks
-   * Each {@link Event | Event} in can have `incomplete` flag to retrieve additional info by user request later
+   * Each {@link Event | Event} can have `incomplete` flag to retrieve additional info by user request later
    *
    * @param args - {@link EventsProviderFetchRequest | EventsProviderFetchRequest}
    * @returns Promise with list of {@link Event | Event}
@@ -165,7 +171,8 @@ export type {
   EventsProviderFetchRequest,
   EventsProviderFetchRequestAfter,
   EventsProviderFetchRequestBefore,
-  EventsProviderUpdateRequest,
+  EventHash,
+  EventStatus,
   EventType,
   Event,
   EventPreview,
